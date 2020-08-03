aacbd4e9e32ea3a812e0fb4d303d09c89409d304
sai-myne <ahlusir@gmail.com>
2020-08-03 13:39:08 +0800

    Removing Records

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -11,6 +11,11 @@
          <li>{{ $topping }}</il>
       @endforeach
     </ul>
+    <form action="/pizzas/{{ $pizza->id }}" method="POST">
+      @csrf
+      @method('DELETE')
+      <button>Complete Order</button>
+    </form>
    </div>
    <a href="/pizzas" class="back"><- Back to all pizzas</a>
 @endsection
